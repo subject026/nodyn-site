98866bddd0a6fc13c6c6b4e89dbea0927a9d8fff
subject026 <subject026@protonmail.com>
2024-04-25 10:47:05 +0100

    init repo

---
+++ vocs.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vocs";
+
+export default defineConfig({
+  title: "nodyn",
+  sidebar: [
+    {
+      text: "Foundry",
+      items: [
+        {
+          text: "forge",
+          link: "/forge",
+        },
+        {
+          text: "cast",
+          link: "/forge",
+        },
+      ],
+    },
+  ],
+});
